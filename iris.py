096d766955776648aa931b8798c75761d3390485
Mason Pelfrey <climbk2@gmail.com>
2018-02-03 11:56:13 -0700

    The completed data science tutorial.

--- iris.py
+++ iris.py
@@ -12,4 +12,26 @@ names = ["Sepal Length", "Sepal Width", "Petal Lenth", "Petal Width", "Class"])
 print(df.head())
 print(df.describe())
 df.hist(bins=20)
-plt.show()
+#plt.show()
+
+data_array = df.values
+np.random.shuffle(data_array)
+X_learning = data_array[:80][:,0:4]
+Y_learning = data_array[:80][:,4]
+
+svc = SVC()
+svc.fit(X_learning,Y_learning)
+
+X = data_array[-20:][:,0:4]
+Y = data_array[-20:][:,4]
+
+predictions = svc.predict(X)
+
+print("Predicted results: ")
+print(predictions)
+print("Acutal Results: ")
+print(Y)
+print("Accuracy rate: %f" % (accuracy_score(Y, predictions)))
+
+print(confusion_matrix(Y, predictions))
+print(classification_report(Y, predictions))
